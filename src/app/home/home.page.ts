f2aa0c92faa0554382d6af4f850024e20e2b4467
Jim Karnopp <jim@kartech.com>
2019-04-26 15:51:02 -0400

    Tracking Changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef,AfterContentInit,OnInit } from '@angular/core';
 import { NavController, Platform } from '@ionic/angular';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Subscription } from 'rxjs';
@@ -12,20 +12,26 @@ declare var google;
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  @ViewChild('map') mapElement: ElementRef;
+export class HomePage implements OnInit, AfterContentInit{
+
+  @ViewChild('mapElement') mapElement: ElementRef;
   map: any;
   currentMapTrack = null;
- 
+ latLng: string;
   isTracking = false;
   trackedRoute = [];
   previousTracks = [];
+  maxZoomService: any;
+  zoom:number = 16;
  
   positionSubscription: Subscription;
  
   constructor(public navCtrl: NavController, private plt: Platform, private geolocation: Geolocation, private storage: Storage) { }
  
-  ionViewDidLoad() {
+  ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
     this.plt.ready().then(() => {
       this.loadHistoricRoutes();
  
@@ -37,14 +43,32 @@ export class HomePage {
         fullscreenControl: false
       }
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      this.maxZoomService = new google.maps.MaxZoomService();
+  
  
       this.geolocation.getCurrentPosition().then((pos) => {
         let latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
         this.map.setCenter(latLng);
-        this.map.setZoom(16);
+        
+        this.map.setZoom(20);
+        console.log("lat: " + pos.coords.latitude);
+        console.log("zoom: " + this.getMaxZoom(latLng));
       }).catch((error) => {
         console.log('Error getting location', error);
+        
       });
+    });
+  }
+
+async getMaxZoom(latLng:any) {
+    var result = await this.maxZoomService.getMaxZoomAtLatLng(latLng, function(response) {
+      console.log(response.zoom);
+      if (response.status === 'OK') {
+        return response.zoom;
+      } 
+      return  16;
+      
     });
   }
  
@@ -68,11 +92,26 @@ export class HomePage {
         setTimeout(() => {
           this.trackedRoute.push({ lat: data.coords.latitude, lng: data.coords.longitude });
           this.redrawPath(this.trackedRoute);
+          this.latLng = "lat: " + data.coords.latitude + " - " + "lng: " + data.coords.longitude;
         }, 0);
       });
  
   }
+
+  stopTracking() {
+    let newRoute = { finished: new Date().getTime(), path: this.trackedRoute };
+    this.previousTracks.push(newRoute);
+    this.storage.set('routes', this.previousTracks);
+   
+    this.isTracking = false;
+    this.positionSubscription.unsubscribe();
+    this.currentMapTrack.setMap(null);
+  }
  
+  showHistoryRoute(route) {
+    this.redrawPath(route);
+  }
+
   redrawPath(path) {
     if (this.currentMapTrack) {
       this.currentMapTrack.setMap(null);
